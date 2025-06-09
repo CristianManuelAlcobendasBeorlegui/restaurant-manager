8ee4200419c3cd887641342c4f99c3bb509b3ae1
Cristian Manuel Alcobendas Beorlegui <cristian.alcobendas@insbaixcamp.cat>
2025-06-09 20:04:28 +0200

    feat: Improve pages title

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Restaurant - Dashboard</title>
     <link rel="icon" type="image/svg+xml" href="{{ asset('img/restaurant-manager-favicon.svg') }}">
     <link
       rel="stylesheet"
--- resources/views/user/join-table.blade.php
+++ resources/views/user/join-table.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Join a table</title>
+    <title>Restaurant - Join a table</title>
     <link rel="icon" type="image/svg+xml" href="{{ asset('img/restaurant-manager-favicon.svg') }}">
     <link rel="stylesheet" href="{{ asset('css/join-table.css') }}" />
     <script src="{{ asset('js/join-table.js') }}"></script>
